6fb8f685c7588a66bee0863e403243898e48f99f
Justin Mills <justin@justinmills.co>
2022-12-27 10:13:03 -0500

    Add files via upload

---
+++ Mood.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+
+
+   /**
+   * @title ContractName
+   * @dev ContractDescription
+   * @custom:dev-run-script file_path
+   */
+
+contract MoodDiary {
+
+    string mood;
+
+    //create a function that writes a mood to the smart contract
+    function setMood(string memory _mood) public {
+        mood = _mood;
+    }
+
+    //create a function that reads the mood from the smart contract  
+    function getMood() public view returns(string memory) {
+        return mood;
+    }
+
+
+
+}
